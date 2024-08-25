6476145290bbf3f5922ecfd9f3d2cf68b49a3d73
dedunipahasara <139765723+dedunipahasara@users.noreply.github.com>
2024-08-25 13:48:02 +0530

    add database

---
+++ java/com.example.poswebback/db.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS pos_web;
+CREATE DATABASE IF NOT EXISTS pos_web;
+SHOW DATABASES;
+USE pos_web;
+
+DROP TABLE IF EXISTS Customer;
+CREATE TABLE IF NOT EXISTS Customer
+(
+    id      VARCHAR(8),
+    name    VARCHAR(30),
+    address VARCHAR(30),
+    salary  double,
+    CONSTRAINT PRIMARY KEY (id)
+);
+SHOW TABLES;
+DESCRIBE Customer;
