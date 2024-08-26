1e2abd6db472d5286da9ec08eeccef6dfaa1e621
dedunipahasara <139765723+dedunipahasara@users.noreply.github.com>
2024-08-26 23:08:52 +0530

    Add itemDao to the code

---
+++ java/com.example.poswebback/dao/custom/ItemDao.java
@@ -0,0 +1,7 @@
+package com.example.poswebback.dao.custom;
+
+import com.example.poswebback.dao.CrudDao;
+import com.example.poswebback.entity.Item;
+
+public interface ItemDao extends CrudDao<Item, String> {
+}
