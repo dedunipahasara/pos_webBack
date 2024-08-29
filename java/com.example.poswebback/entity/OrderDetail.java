406b6078a7710507869a1a33710b23cf11d26ff9
dedunipahasara <139765723+dedunipahasara@users.noreply.github.com>
2024-08-29 13:05:51 +0530

    Add files via upload

---
+++ java/com.example.poswebback/entity/OrderDetail.java
@@ -0,0 +1,16 @@
+package com.example.poswebback.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetail {
+    private String orderId;
+    private String itemCode;
+    private int qty;
+    private double total;
+
+}
---
+++ java/com.example.poswebback/entity/Orders.java
@@ -0,0 +1,16 @@
+package com.example.poswebback.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Orders {
+    
+    private String orderId;
+    private String orderDate;
+    private String cusId;
+}
